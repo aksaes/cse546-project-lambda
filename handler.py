6e07a96135b1e05ec6184d9b825572ad4506308f
aksaes <aksaelizabeth28@gmail.com>
2023-11-02 11:55:19 -0700

    Add code to get item from DB

--- handler.py
+++ handler.py
@@ -9,6 +9,9 @@ input_bucket = "input-bucket-zxz"
 output_bucket = "output-bucket-zxz"
 
 s3 = boto3_client('s3')
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('StudentData')
 
 # Function to read the 'encoding' file
 def open_encoding(filename):
@@ -16,6 +19,19 @@ def open_encoding(filename):
 	data = pickle.load(file)
 	file.close()
 	return data
+
+def get_DBitem(item):
+	try:
+		response = table.get_item(Key={'name': match_name})
+		if 'Item' in response:
+			print('Item found:', response['Item'])
+			return response['Item']
+		else:
+			print('No item found with the primary key:', primary_key_value)
+			return -1
+	except Exception as e:
+		print('Error fetching item from DynamoDB: ', e)
+		return -1
 
 def face_recognition_handler(event, context):	
 	bucket = event['Records'][0]['s3']['bucket']['name']
@@ -36,3 +52,14 @@ def face_recognition_handler(event, context):
 	scores = face_recognition.api.face_distance(encodings['encoding'], face_encoding)
 	match_idx = np.argmin(scores)
 	match_name = encodings['name'][match_idx]
+
+	# Fetch record from DB
+	item = get_DBitem(match_name)
+
+	if item == -1:
+		return
+
+	print(item)
+
+
+	
