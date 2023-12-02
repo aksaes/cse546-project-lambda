5d5c332dbb53f07dc5c4c22e62500e16c5fe2b21
saranzxz <124385237+saranzxz@users.noreply.github.com>
2023-12-01 21:23:48 -0700

    Update index.py

--- face-recognition/index.py
+++ face-recognition/index.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 class Event:
     def __init__(self):
-        self.body = request.get_json()
+        self.body = request.get_data()
         # self.headers = request.headers
         # self.method = request.method
         # self.query = request.args
